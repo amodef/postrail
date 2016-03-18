e99141e0ffd0050a600e9ff3988f28865b7d1cfc
Julien Gascard <julien@gascard.eu>
2016-03-18 20:06:31 +0100

    Adapt the app to use external subscription service

--- app/routes.php
+++ app/routes.php
@@ -7,7 +7,7 @@ $app->get('/about', 'App\Controllers\StaticController:about')->setName('about');
 
 // runner routes
 $app->get('/runner', 'App\Controllers\RunnerController:index')->setName('runner.index');
-$app->get('/runner/create', 'App\Controllers\RunnerController:create')->setName('runner.create');
-$app->post('/runner', 'App\Controllers\RunnerController:store')->setName('runner.store');
-$app->get('/runner/{runner:[0-9]+}', 'App\Controllers\RunnerController:show')->setName('runner.show');
-$app->delete('/runner/{runner:[0-9]+}', 'App\Controllers\RunnerController:delete')->setName('runner.delete');
+//$app->get('/runner/create', 'App\Controllers\RunnerController:create')->setName('runner.create');
+//$app->post('/runner', 'App\Controllers\RunnerController:store')->setName('runner.store');
+//$app->get('/runner/{runner:[0-9]+}', 'App\Controllers\RunnerController:show')->setName('runner.show');
+//$app->delete('/runner/{runner:[0-9]+}', 'App\Controllers\RunnerController:delete')->setName('runner.delete');
